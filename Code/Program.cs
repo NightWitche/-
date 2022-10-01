7325c708ad24072a3a9c53cee8bf8672228b74f5
Margo <rita-1991@mail.ru>
2022-10-02 01:40:48 +0300

    Code operation2

--- Code/Program.cs
+++ Code/Program.cs
@@ -6,54 +6,66 @@
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
 
-string[] array1 = { "Hello", "2", "world", ":-)" };
-string[] array2 = { "1234", "1567", "-2", "computer science" };
-string[] array3 = { "Russia", "Denmark", "Kazan" };
-
-int maxLenght = 3;
-
-string[] GetShortWordsArray(string[] array)
-{
-    int length = array.Length;
-    string[] result = new string[length];
-    int count = 0;
-
-    for (int i = 0; i < length; i++)
-    {
-        if (array[i].Length <= maxLenght)
-        {
-            result[count] = array[i];
-            count++;
-        }
-    }
-    Array.Resize(ref result, count);
-
-    return result;
-}
-
-void PrintArray(string[] array)
-{
-    if (array.Length == 0)
-    {
-        Console.WriteLine("Array is empty");
-    }
-    else
-    {
-        Array.ForEach(array, (str) => Console.Write($"{str} "));
-        Console.WriteLine();
-    }
-}
-
-void PrintTask(string[] array)
-{
-    Console.WriteLine("Source array:");
-    PrintArray(array);
-    string[] shortArray = GetShortWordsArray(array);
-    Console.WriteLine($"Result array (words are less or equal than {maxLenght} symbols)");
-    PrintArray(shortArray);
-    Console.WriteLine();
-}
-
-PrintTask(array1);
-PrintTask(array2);
-PrintTask(array3);
+Console.WriteLine($"Введите количество элементов массива:\t");  
+int lengthOfArray = Convert.ToInt32(Console.ReadLine());  
+string[] array = CreateStrArray(lengthOfArray);  
+ 
+int maxLenght = 3; 
+  
+string[] CreateStrArray(int length)  
+{  
+    string[] arr = new string[length];  
+  
+    Console.WriteLine($"Введите элементы массива:");  
+  
+    for (int i = 0; i < length; i++)  
+    {  
+        arr[i] = Console.ReadLine()!;  
+    }  
+  
+    return arr;  
+} 
+ 
+string[] GetShortWordsArray(string[] array) 
+{ 
+    int length = array.Length; 
+    string[] result = new string[length]; 
+    int count = 0; 
+ 
+    for (int i = 0; i < length; i++) 
+    { 
+        if (array[i].Length <= maxLenght) 
+        { 
+            result[count] = array[i]; 
+            count++; 
+        } 
+    } 
+    Array.Resize(ref result, count); 
+ 
+    return result; 
+} 
+ 
+void PrintArray(string[] array) 
+{ 
+    if (array.Length == 0) 
+    { 
+        Console.WriteLine("Array is empty"); 
+    } 
+    else 
+    { 
+        Array.ForEach(array, (str) => Console.Write($"{str} ")); 
+        Console.WriteLine(); 
+    } 
+} 
+ 
+void PrintTask(string[] array) 
+{ 
+    Console.WriteLine("\nИсходный массив:\n"); 
+    PrintArray(array); 
+    string[] shortArray = GetShortWordsArray(array); 
+    Console.WriteLine($"\nРезультат (элементы длиной менее или равной {maxLenght} символам):\n"); 
+    PrintArray(shortArray); 
+    Console.WriteLine(); 
+} 
+ 
+PrintTask(array);
